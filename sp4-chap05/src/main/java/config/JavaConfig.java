bbba4bdc778934500182092afdd3cd31a3208665
madvirus <madvirus@madvirus.net>
2014-10-27 11:08:04 +0900

    java config

---
+++ sp4-chap05/src/main/java/config/JavaConfig.java
@@ -0,0 +1,36 @@
+package config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import spring.MemberDao;
+import spring.MemberInfoPrinter;
+import spring.MemberPrinter;
+import spring.MemberRegisterService;
+
+@Configuration
+public class JavaConfig {
+
+	@Bean
+	public MemberDao memberDao() {
+		return new MemberDao();
+	}
+
+	@Bean
+	public MemberRegisterService memberRegSvc() {
+		return new MemberRegisterService(memberDao());
+	}
+
+	@Bean
+	public MemberPrinter printer() {
+		return new MemberPrinter();
+	}
+
+	@Bean
+	public MemberInfoPrinter infoPrinter() {
+		MemberInfoPrinter infoPrinter = new MemberInfoPrinter();
+		infoPrinter.setMemberDao(memberDao());
+		infoPrinter.setPrinter(printer());
+		return infoPrinter;
+	}
+}
