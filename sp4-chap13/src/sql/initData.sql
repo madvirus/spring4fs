d9b0a955ea928c79f244771e4f360f674aaee06a
madvirus <madvirus@madvirus.net>
2014-12-09 23:32:18 +0900

    chap13

---
+++ sp4-chap13/src/sql/initData.sql
@@ -0,0 +1,6 @@
+truncate table spring4fs.MEMBER;
+
+insert into spring4fs.MEMBER values (1, 'madvirus@madvirus.net', '1234', '최범균', '2014-12-01 13:01:02');
+insert into spring4fs.MEMBER values (2, 'bkchoi@bkchio.com', '5678', '최BK', '2014-12-03 18:31:49');
+insert into spring4fs.MEMBER values (3, 'gildong@some.com', '1234', '홍길동', '2014-11-19 00:21:02');
+insert into spring4fs.MEMBER values (4, 'kingsejon@chosunking.org', '1234', '세종대왕', '2014-12-08 19:00:00');
